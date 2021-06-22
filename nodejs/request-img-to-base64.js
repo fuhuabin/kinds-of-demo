71d501d895b9466606aecd0e9d3679d87f01f750
fuhb <fuhb@sutpc.com>
2021-06-22 17:02:51 +0800

    feat: add request img to base64 demo

---
+++ nodejs/request-img-to-base64.js
@@ -0,0 +1,30 @@
+const https = require('https');
+
+/**
+ * use this method to get a promise that return the base64 string of an image
+ * if the protocol is http,use http library,like this: const http = require('http'); http.get...
+ * @param {img url} url 
+ * @returns 
+ */
+const getImg = function(url) {
+    return new Promise(resolve => {
+        https.get(url,function(res){
+        　　 let chunks = []; //用于保存网络请求不断加载传输的缓冲数据
+        　　 let size = 0;　　 //保存缓冲数据的总长度
+        
+        　　 res.on('data',function(chunk){
+        　　　　  chunks.push(chunk);　 
+        　　　　  size += chunk.length;　　//累加缓冲数据的长度
+        　　 });
+        
+        　　 res.on('end',function(err){
+                let data = Buffer.concat(chunks, size);
+                resolve(data.toString('base64'));
+            });
+        });
+    })
+}
+
+getImg('xxx').then(base64Str => {
+    // use the base64 string here
+});
